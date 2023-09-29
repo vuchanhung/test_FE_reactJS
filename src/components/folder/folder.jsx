e2a0ab631b4c10a5683fd437efd54e30028b7542
Admin <vuchanhung2001@gmail.com>
2023-09-30 00:29:17 +0700

    update bundle widget on html and update background color

--- src/components/folder/folder.jsx
+++ src/components/folder/folder.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const folder = () => {
   return (
     <div>
-      
+      <p>Testing</p>
     </div>
   )
 }
--- src/page/Home/home.jsx
+++ src/page/Home/home.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import './home.css'
-
+import Folder from '../../components/folder/folder'
 const home = () => {
   return (
     <>
       <div className="home--container">
         {/* title content */}
+
         <div className="title--container">
           <span className="content-1">Welcome to</span>
           <br />
           <span className="content-2">GrainLogic!</span>
         </div>
+
         <div className="imgs--widgets--container">
           {/* img home on the left */}
           <div className="img--container">
             <div className="grid--img">
-              <div className="shop--img"></div>
+              <div className="shop--img">
+              </div>
             </div>
           </div>
+          
+          {/*bundles widget on the right  */}
+          <div className="bundle--widget">
+            <Folder />
+          </div>
         </div>
-        {/*bundles widget on the right  */}
-        <div className="bundle--widget"></div>
       </div>
     </>
 
