d48def84093c942cee33dedeff5c215ba3953b1a
Admin <vuchanhung2001@gmail.com>
2023-09-30 01:32:53 +0700

    donwload and setup folder for bundle images

---
+++ src/assets/data/bundles.js
@@ -0,0 +1,14 @@
+// import product_category from '../images/product_category.png'
+// import company from '../images/company.png'
+// import add_subscription from '../images/add_subscription.png'
+// import public_information from '../images/public_information.png'
+// import business_information from '../images/business_information.png'
+// import connection from '../images/connection.png'
+// import online_shop from '../images/online_shop.png'
+// import product_dashboard from '../images/product_dashboard.png'
+// import offer_dashboard from '../images/offer_dashboard.png'
+// import developer from '../images/developer.png'
+// import message from '../images/message.png'
+// import logout from '../images/logout.png'
+
+
