8732f2abb130cfa92e85d6b849c89deedc6a30e8
Admin <vuchanhung2001@gmail.com>
2023-09-29 23:51:39 +0700

    split App code to home page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import './App.css';
-import React from 'react';
-
+import "./App.css";
+import React from "react";
+import Home from './page/Home/home'
 function App() {
   return (
-    <div className='App'>
-    {/* left home screen */}
-      
-        <div className='home--content'>
-          <div className='title--content'>
-            <h1 className='section--title'>
-              <span className='content-1'>Welcome to</span>
-              <br />
-              <span className='content-2'>GrainLogic!</span>
-            </h1>
-          </div>
-          <div className='img--container'>
-              <div className='grid--img'>
-              <div className='shop--img'></div>
-          </div>
-        </div>
-      {/* right home screen */}
-        <div className=''>
-        </div>
-      </div>
-  </div>
+    <div className="App">
+      <Home/>
+    </div>
   );
 }
 
--- src/page/Home/home.jsx
+++ src/page/Home/home.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import './home.css'
-import shopImgEx from '../../assets/image_632.jpg'
 
 const home = () => {
   return (
     <>
-      
+      <div className="home--container">
+        {/* title content */}
+        <div className="title--container">
+          <span className="content-1">Welcome to</span>
+          <br />
+          <span className="content-2">GrainLogic!</span>
+        </div>
+        <div className="imgs--widgets--container">
+          {/* img home on the left */}
+          <div className="img--container">
+            <div className="grid--img">
+              <div className="shop--img"></div>
+            </div>
+          </div>
+        </div>
+        {/*bundles widget on the right  */}
+        <div className="bundle--widget"></div>
+      </div>
     </>
-    
+
   )
 }
 
